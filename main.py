61cb0bb03e6d1fbcf30d9796b3d778a570f8a102
by17s <give.56.com@gmail.com>
2025-07-03 17:45:44 +0500

    Fix name

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 if __name__ == "__main__":
     import sys
-    from src import lxgen
+    from src import LxGen
     
     if len(sys.argv) < 2:
         print("Usage: python main.py <input_file>")
         sys.exit(1)
         
-    lg = lxgen.LxGen(sys.argv[1])
+    lg = LxGen.LxGen(sys.argv[1])
